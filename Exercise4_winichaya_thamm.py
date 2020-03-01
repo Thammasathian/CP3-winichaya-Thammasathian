0fcff643d8c3054d937ea933306327bf3840b9d9
Thammasathian <winichaya150741@gmail.com>
2020-03-01 17:43:13 +0700

    Exercise4_winichaya_thamm

---
+++ Exercise4_winichaya_thamm.py
@@ -0,0 +1,13 @@
+#Foundation English
+FE = float(input("score:"))
+#General Business
+GB = float(input("score:"))
+#Introduction to Computer Systems
+IS = float(input("score:"))
+#Computer Programming
+CP = float(input("score:"))
+
+print("Foundation English:",FE,"score" )
+print("General Business:",GB,"score")
+print("Introduction to Computer Systems:",IS,"score")
+print("Computer Programming:",CP,"score")
